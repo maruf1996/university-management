36f8ca073dae48caa3910b9b560817902605b30d
maruf1996 <maruf3813@gmail.com>
2023-08-14 18:15:00 +0600

    'faculty-admin-success'

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -17,4 +17,10 @@ router.post(
   UserController.createFaculy,
 );
 
+router.post(
+  '/create-admin',
+  validateRequest(UserValidation.createAdminZodSchema),
+  UserController.createAdmin,
+);
+
 export const UserRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+import { AdminRoutes } from '../modules/admin/admin.route';
 import { FacultyRoutes } from '../modules/faculty/faculty.route';
+import { ManagementDepartmentRoutes } from '../modules/managementDepartment/managementDepartment.route';
 import { StudentRoutes } from '../modules/student/student.route';
 import { UserRoutes } from '../modules/user/user.route';
 
@@ -32,6 +34,14 @@ const moduleRoutes = [
   {
     path: '/faculties',
     route: FacultyRoutes,
+  },
+  {
+    path: '/management-departments',
+    route: ManagementDepartmentRoutes,
+  },
+  {
+    path: '/admins',
+    route: AdminRoutes,
   },
 ];
 
